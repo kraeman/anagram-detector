cbcaed6d9727b3f7c168c1e1e4818ff6c01acdb0
krae <kraewind@gmail.com>
2020-11-02 15:54:59 -0500

    commit tree d97b73c1ac20818d420c6508a4077eb9457295f1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        sorted = @word.split("").sort
+        new_array = []
+        array.each do |i|
+            if i.split("").sort == sorted && (i.length == @word.length)
+                new_array.push(i)
+            end
+
+        end
+        new_array
+    end
+
+    attr_accessor :word
+    
+end
